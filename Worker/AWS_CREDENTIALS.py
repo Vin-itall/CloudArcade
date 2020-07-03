2cfe857d86a8f8a77f46925ec987adb44aef9a83
Vinit Acharya <vin0itall@gmail.com>
2020-07-03 16:42:33 -0700

    Update AWS_CREDENTIALS.py

--- Worker/AWS_CREDENTIALS.py
+++ Worker/AWS_CREDENTIALS.py
@@ -1,2 +1,2 @@
-aws_access_key_id = 'AKIAQ7PPHJ5MGBKCJA7G',
-aws_secret_access_key = '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'
+aws_access_key_id = '*******************',
+aws_secret_access_key = '****************************'
